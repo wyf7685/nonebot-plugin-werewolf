92695510e9ab795e8bf363018041cab97541d04a
wyf7685 <wyf7685@163.com>
2024-09-03 21:30:08 +0800

    调试配置

---
+++ scripts/bot.py
@@ -0,0 +1,25 @@
+import shutil
+from pathlib import Path
+
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter as OB11Adapter
+
+
+def clean_pycache(path: Path = Path()):
+    for p in path.iterdir():
+        if not p.is_dir():
+            continue
+        if p.name == "__pycache__":
+            shutil.rmtree(p)
+        else:
+            clean_pycache(p)
+
+
+nonebot.init()
+driver = nonebot.get_driver()
+driver.register_adapter(OB11Adapter)
+nonebot.load_from_toml("pyproject.toml")
+
+if __name__ == "__main__":
+    nonebot.run()
+    clean_pycache()
