d84617ada5181ec087b2b5ad2e8daabe3318e0b4
wyf7685 <wyf7685@163.com>
2024-09-05 16:16:27 +0800

    设置小丑概率范围, 修改默认值

--- nonebot_plugin_werewolf/config.py
+++ nonebot_plugin_werewolf/config.py
@@ -1,5 +1,5 @@
 from nonebot import get_plugin_config
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from .constant import Role
 
@@ -9,7 +9,7 @@ class PluginConfig(BaseModel):
     role_preset: list[tuple[int, int, int, int]] | None = None
     werewolf_priority: list[Role] | None = None
     priesthood_proirity: list[Role] | None = None
-    joker_probability: float = 0.3
+    joker_probability: float = Field(default=0.0, ge=0.0, le=1.0)
 
 
 class Config(BaseModel):
