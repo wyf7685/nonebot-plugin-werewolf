3ff86a18c190523e51b688f34c58bd76b4bee8a0
wyf7685 <wyf7685@163.com>
2024-10-23 13:32:28 +0800

    fix start game

--- nonebot_plugin_werewolf/matchers/start_game.py
+++ nonebot_plugin_werewolf/matchers/start_game.py
@@ -32,7 +32,7 @@ async def handle_start_warning(target: MsgTarget) -> None:
 
 
 async def _prepare_receive(
-    stream: MemoryObjectSendStream[tuple[str, str, str]],
+    stream: MemoryObjectSendStream[tuple[Event, str, str]],
     event: Event,
     group: Target,
 ) -> None:
@@ -44,111 +44,104 @@ async def _prepare_receive(
         keep_session=False,
         rule=to_me() & same_group & rule_not_in_game,
     )
-    def wait(
-        event: Event,
-        msg: UniMsg,
-        session: Uninfo,
-    ) -> tuple[str, str, str]:
-        user_id = event.get_user_id()
-        name = extract_session_member_nick(session) or user_id
-        return (
-            user_id,
-            msg.extract_plain_text().strip(),
-            name,
-        )
+    def wait(event: Event, msg: UniMsg, session: Uninfo) -> tuple[Event, str, str]:
+        text = msg.extract_plain_text().strip()
+        name = extract_session_member_nick(session) or event.get_user_id()
+        return (event, text, name)
 
-    async for user, text, name in wait(default=(None, "", "")):
-        if user is None:
+    async for evt, text, name in wait(default=(None, "", "")):
+        if evt is None:
             continue
-        await stream.send((user, text, re.sub(r"[\u2066-\u2069]", "", name)))
+        await stream.send((evt, text, re.sub(r"[\u2066-\u2069]", "", name)))
 
 
 async def _prepare_handle(
-    stream: MemoryObjectReceiveStream[tuple[str, str, str]],
+    stream: MemoryObjectReceiveStream[tuple[Event, str, str]],
     players: dict[str, str],
     admin_id: str,
     finished: anyio.Event,
 ) -> None:
     logger = nonebot.logger.opt(colors=True)
 
+    async def send(msg: str, /) -> None:
+        await UniMessage.text(msg).send(target=event, reply_to=True)
+
     while True:
-        user, text, name = await stream.receive()
-        msg = UniMessage.at(user).text("\n")
-        colored = f"<y>{escape_tag(name)}</y>(<c>{escape_tag(user)}</c>)"
+        event, text, name = await stream.receive()
+        user_id = event.get_user_id()
+        colored = f"<y>{escape_tag(name)}</y>(<c>{escape_tag(user_id)}</c>)"
 
         # 更新用户名
         # 当用户通过 chronoca:poke 加入游戏时, 插件无法获取用户名, 原字典值为用户ID
-        if user in players:
-            players[user] = name
+        if players.get(user_id) != name:
+            logger.debug(f"更新玩家显示名称: {colored}")
+            players[user_id] = name
 
-        match (text, user == admin_id):
+        match (text, user_id == admin_id):
             case ("开始游戏", True):
                 player_num = len(players)
                 role_preset = config.get_role_preset()
                 if player_num < min(role_preset):
-                    await (
-                        msg.text(f"⚠️游戏至少需要 {min(role_preset)} 人, ")
-                        .text(f"当前已有 {player_num} 人")
-                        .send()
+                    await send(
+                        f"⚠️游戏至少需要 {min(role_preset)} 人, "
+                        f"当前已有 {player_num} 人"
                     )
                 elif player_num > max(role_preset):
-                    await (
-                        msg.text(f"⚠️游戏最多需要 {max(role_preset)} 人, ")
-                        .text(f"当前已有 {player_num} 人")
-                        .send()
+                    await send(
+                        f"⚠️游戏最多需要 {max(role_preset)} 人, "
+                        f"当前已有 {player_num} 人"
                     )
                 elif player_num not in role_preset:
-                    await (
-                        msg.text(f"⚠️不存在总人数为 {player_num} 的预设, ")
-                        .text("无法开始游戏")
-                        .send()
-                    )
+                    await send(f"⚠️不存在总人数为 {player_num} 的预设, 无法开始游戏")
                 else:
-                    await msg.text("✏️游戏即将开始...").send()
+                    await send("✏️游戏即将开始...")
                     logger.info(f"游戏发起者 {colored} 开始游戏")
                     finished.set()
-                    players["#$start_game$#"] = user
+                    players["#$start_game$#"] = user_id
                     return
 
             case ("开始游戏", False):
-                await msg.text("⚠️只有游戏发起者可以开始游戏").send()
+                await send("⚠️只有游戏发起者可以开始游戏")
 
             case ("结束游戏", True):
                 logger.info(f"游戏发起者 {colored} 结束游戏")
-                await msg.text("ℹ️已结束当前游戏").send()
+                await send("ℹ️已结束当前游戏")
                 finished.set()
                 return
 
             case ("结束游戏", False):
-                await msg.text("⚠️只有游戏发起者可以结束游戏").send()
+                await send("⚠️只有游戏发起者可以结束游戏")
 
             case ("加入游戏", True):
-                await msg.text("ℹ️游戏发起者已经加入游戏了").send()
+                await send("ℹ️游戏发起者已经加入游戏了")
 
             case ("加入游戏", False):
-                if user not in players:
-                    players[user] = name
+                if user_id not in players:
+                    players[user_id] = name
                     logger.info(f"玩家 {colored} 加入游戏")
-                    await msg.text("✅成功加入游戏").send()
+                    await send("✅成功加入游戏")
                 else:
-                    await msg.text("ℹ️你已经加入游戏了").send()
+                    await send("ℹ️你已经加入游戏了")
 
             case ("退出游戏", True):
-                await msg.text("ℹ️游戏发起者无法退出游戏").send()
+                await send("ℹ️游戏发起者无法退出游戏")
 
             case ("退出游戏", False):
-                if user in players:
-                    del players[user]
+                if user_id in players:
+                    del players[user_id]
                     logger.info(f"玩家 {colored} 退出游戏")
-                    await msg.text("✅成功退出游戏").send()
+                    await send("✅成功退出游戏")
                 else:
-                    await msg.text("ℹ️你还没有加入游戏").send()
+                    await send("ℹ️你还没有加入游戏")
 
             case ("当前玩家", _):
-                msg.text("✨当前玩家:\n")
-                for idx, user_id in enumerate(players, 1):
-                    msg.text(f"\n{idx}. {players[user_id]}")
-                await msg.send()
+                await send(
+                    "✨当前玩家:\n"
+                    + "\n".join(
+                        f"{idx}. {players[user_id]}"
+                        for idx, user_id in enumerate(players, 1)
+                    )
+                )
 
 
 async def prepare_game(event: Event, players: dict[str, str]) -> None:
@@ -157,15 +150,15 @@ async def prepare_game(event: Event, players: dict[str, str]) -> None:
     Game.starting_games[group] = players
 
     finished = anyio.Event()
-    send, recv = anyio.create_memory_object_stream()
+    send, recv = anyio.create_memory_object_stream[tuple[Event, str, str]]()
 
-    async def handle_cancel() -> None:
+    async def _handle_cancel() -> None:
         await finished.wait()
         tg.cancel_scope.cancel()
 
     try:
         async with anyio.create_task_group() as tg:
-            tg.start_soon(handle_cancel)
+            tg.start_soon(_handle_cancel)
             tg.start_soon(_prepare_receive, send, event, group)
             tg.start_soon(_prepare_handle, recv, players, admin_id, finished)
     except Exception as err:
