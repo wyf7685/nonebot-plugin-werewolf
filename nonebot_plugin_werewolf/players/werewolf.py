b3b9a9793e457ee48b66712ec85fcb1aaa8663e5
wyf7685 <wyf7685@163.com>
2025-01-27 15:47:53 +0800

    relative import

--- nonebot_plugin_werewolf/players/werewolf.py
+++ nonebot_plugin_werewolf/players/werewolf.py
@@ -6,8 +6,7 @@ import anyio
 import nonebot
 from nonebot_plugin_alconna.uniseg import UniMessage
 
-from nonebot_plugin_werewolf.config import GameBehavior
-
+from ..config import GameBehavior
 from ..constant import STOP_COMMAND, stop_command_prompt
 from ..models import Role, RoleGroup
 from ..utils import ObjectStream, check_index
