ac017cf74cecc16d16eb3f28ebfcf6f949eaecc9
wyf7685 <wyf7685@163.com>
2025-04-20 19:54:36 +0800

    refactor config

--- nonebot_plugin_werewolf/config.py
+++ nonebot_plugin_werewolf/config.py
@@ -18,31 +18,31 @@ from .models import Role
 
 
 class ConfigFile(BaseModel):
-    FILE: ClassVar[Path]
-    _cache: ClassVar[Self | None] = None
+    _file_: ClassVar[Path]
+    _cache_: ClassVar[Self | None] = None
 
     def __init_subclass__(cls, **kwargs: Any) -> None:  # noqa: ANN401
         super().__init_subclass__(**kwargs)
-        if not cls.FILE.exists():
+        if not cls._file_.exists():
             cls().save()
 
     @classmethod
     def load(cls) -> Self:
-        return type_validate_json(cls, cls.FILE.read_text())
+        return type_validate_json(cls, cls._file_.read_text())
 
     @classmethod
     def get(cls, *, use_cache: bool = True) -> Self:
-        if cls._cache is None or not use_cache:
-            cls._cache = cls.load()
-        return cls._cache
+        if cls._cache_ is None or not use_cache:
+            cls._cache_ = cls.load()
+        return cls._cache_
 
     def save(self) -> None:
-        self.FILE.write_text(json.dumps(model_dump(self)))
-        type(self)._cache = self  # noqa: SLF001
+        self._file_.write_text(json.dumps(model_dump(self)))
+        type(self)._cache_ = self
 
 
 class PresetData(ConfigFile):
-    FILE: ClassVar[Path] = get_plugin_data_file("preset.json")
+    _file_: ClassVar[Path] = get_plugin_data_file("preset.json")
 
     role_preset: dict[int, tuple[int, int, int]] = DEFAULT_ROLE_PRESET.copy()
     werewolf_priority: list[Role] = DEFAULT_WEREWOLF_PRIORITY.copy()
@@ -50,36 +50,33 @@ class PresetData(ConfigFile):
     jester_probability: float = Field(default=0.0, ge=0.0, le=1.0)
 
 
+class _Timeout(BaseModel):
+    prepare: int = Field(default=5 * 60, ge=5 * 60)
+    speak: int = Field(default=60, ge=60)
+    group_speak: int = Field(default=120, ge=120)
+    interact: int = Field(default=60, ge=60)
+    vote: int = Field(default=60, ge=60)
+    werewolf: int = Field(default=120, ge=120)
+
+    @property
+    def speak_timeout_prompt(self) -> str:
+        return f"限时{self.speak / 60:.1f}分钟, 发送 “{stop_command_prompt()}” 结束发言"
+
+    @property
+    def group_speak_timeout_prompt(self) -> str:
+        return (
+            f"限时{self.group_speak / 60:.1f}分钟, "
+            f"全员发送 “{stop_command_prompt()}” 结束发言"
+        )
+
+
 class GameBehavior(ConfigFile):
-    FILE: ClassVar[Path] = get_plugin_data_file("behavior.json")
+    _file_: ClassVar[Path] = get_plugin_data_file("behavior.json")
 
     show_roles_list_on_start: bool = False
     speak_in_turn: bool = False
     dead_channel_rate_limit: int = 8  # per minute
     werewolf_multi_select: bool = False
-
-    class _Timeout(BaseModel):
-        prepare: int = Field(default=5 * 60, ge=5 * 60)
-        speak: int = Field(default=60, ge=60)
-        group_speak: int = Field(default=120, ge=120)
-        interact: int = Field(default=60, ge=60)
-        vote: int = Field(default=60, ge=60)
-        werewolf: int = Field(default=120, ge=120)
-
-        @property
-        def speak_timeout_prompt(self) -> str:
-            return (
-                f"限时{self.speak / 60:.1f}分钟, "
-                f"发送 “{stop_command_prompt()}” 结束发言"
-            )
-
-        @property
-        def group_speak_timeout_prompt(self) -> str:
-            return (
-                f"限时{self.group_speak / 60:.1f}分钟, "
-                f"全员发送 “{stop_command_prompt()}” 结束发言"
-            )
-
     timeout: Final[_Timeout] = _Timeout()
 
 
